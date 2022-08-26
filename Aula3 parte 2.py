b60068be80d945f7d81e5445438955847f87c8c3
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-26 08:33:17 -0300

    Create Aula3 parte 2.py

---
+++ Aula3 parte 2.py
@@ -0,0 +1,22 @@
+a = int(input('Primeiro bimestre: '))
+b = int(input('Segundo bimestre:'))
+c = int(input('Terceiro bimestre:'))
+d = int(input('Quarto bimestre:'))
+if a > 10:
+      a = int(input('Você digitou errado. Primeiro bimestre: '))
+if b > 10:
+      b = int(input('Você digitou errado. Segundo bimestre: '))
+if c > 10:
+     c = int(input('Você digitou errado. Terceiro bimestre: '))
+if d > 10:
+     d = int(input('Você digitou errado. Quarto bimestre: '))     
+
+
+
+
+
+#media = (a + b + c + d) / 4
+#if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+  #  print('media: {}'.format(media))
+#else: 
+ #   print('foi informado alguma nota errada')
