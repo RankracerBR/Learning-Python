b2470804a037ce0c8474a77c757308d4aced1080
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-26 08:36:49 -0300

    Create Lacos de repeticao.py

---
+++ Lacos de repeticao.py
@@ -0,0 +1,14 @@
+nota = int(input('Entre com a nota: '))
+while nota > 10:
+    nota = int(input('Nota inválida. Entre com a nota correta: '))
+
+      
+   # for x in range(1, a+1): 
+ # resto = a % x
+   # print(x, resto)
+    #if resto == 0:
+    #    div += 1
+#if div == 2:
+ #   print('número {} é primo'.format(a))
+#else: 
+ #   print('número {} não é primo'.format) 
