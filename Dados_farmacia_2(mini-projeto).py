44e0e4ce9e60b47ba2934d02bcce54452bda0307
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-06 15:47:44 -0300

    Update Dados_farmacia_2(mini-projeto).py

--- Dados_farmacia_2(mini-projeto).py
+++ Dados_farmacia_2(mini-projeto).py
@@ -1,4 +1,5 @@
 import os
+
 tamanho = os.get_terminal_size()
 largura = tamanho[0]
 
@@ -15,21 +16,28 @@ tarjas =  ["sem tarja","tarja_amarela", "tarja_vermelha", "tarja_preta"]
 conjunto_1 = [sem_tarja,tarja_amarela,tarja_vermelha,tarja_preta]
 conjunto_2 = [tarjas]
 op = 1
-tamanho = os.get_terminal_size()
-largura = tamanho[0]
+
+
+
 while op != 0:
-    print('_'.ljust(45))
+    print('_'*200)
     print('Jampy'.center(largura))
     print('Menu de opções: ')
     print('1-Procurar remédio, 2-Ver tarjas, 3-sair')
-    print('_'.ljust(45))
+    print('_'*200)
     pedido = int(input('Olá, seja bem-vindo a Farmácia Jampy, posso ajudar:(pressiona 1, 2 ou 3 para selecionar): '))
+
+    
+    
     if pedido == 1:
-        print('Temos os seguintes remédios: ')
-        print(f'{conjunto_1}')
+       print('Temos os seguintes remédios: ')
+       print(f'{sem_tarja}') 
+       print(f'{tarja_amarela}') 
+       print(f'{tarja_vermelha}')
+       print(f'{tarja_preta}') 
     elif pedido == 2:
-        
-        print(f'Temos as seguintes tarjas: {conjunto_2}')
+       print('Temos as seguintes cargas: {}'.format(conjunto_2)) 
+
     elif pedido == 3:
         
         print('Até breve')
