2fd45c4da49802e06574685a46b75eee467ce4eb
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-18 12:34:42 -0300

    Create gta vi.py

---
+++ gta vi.py
@@ -0,0 +1,15 @@
+import time
+ld= '🏃'*1
+ld2 = '🏃'*5
+ld3 = '🏃'*1
+vento1 = '💨'*1
+vento2 = '💨'*5
+vento3 ='💨'*1
+plc = '🚓'
+op = 1
+print('GTA VI')
+while op !=0:  
+    time.sleep(0.5)
+    print(f'{ld}{vento1}{plc}')
+    print(f'{ld2}{vento2}{plc}')    
+    print(f'{ld3}{vento3}{plc}')
