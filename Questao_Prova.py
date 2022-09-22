e788704d69393f6b4b09c5fb6b18e754af394b92
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-22 20:28:20 -0300

    Update Questao_Prova.py

--- Questao_Prova.py
+++ Questao_Prova.py
@@ -1,16 +1,16 @@
 # Contar a quantidade de passos percorridos em números pares e impares
 n = int(input())
-passos = 0
-while 1 <= n:
-    if n % 2 ==0:
-        n = n - 2
-        n / 2
-        passos = passos + 1
-        print(f'{passos}. {n}')
-    if n % 1 == 0:
-        n = n - 1
-        (n * 3) + 1
-        passos = passos + 1
-        print(f'{passos}. {n}')
+passos = 0 
+
+if n > 0:
+  while n > 1:
+    if n % 2 == 0:
+      n = n /2 
+    else:
+      n = 3*n + 1
+      passos += 1
+  print(passos)
+else:
+  print('impossível calcular')
 
     
