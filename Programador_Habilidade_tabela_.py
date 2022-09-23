752128a20ab7e190cfa22916adf03738b358d3f9
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-23 01:40:09 -0300

    Update Programador_Habilidade_tabela_.py

--- Programador_Habilidade_tabela_.py
+++ Programador_Habilidade_tabela_.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship #fazer consultas no banco de dados
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from  sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///habilidades.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -9,23 +9,29 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 class Programadores(Base):
-    __tablename__ = 'programadores'
+    __tablename__='programadores'
     id = Column(Integer, primary_key=True)
     nome = Column(String(40), index=True)
     idade = Column(Integer)
-    email = Column(String)
+    email = Column(String(40))   
     def __repr__(self):
-        return '<Programador(a) {}, Idade {}>'.format(self.nome,self.idade)
+        return '<Programadores: {}'.format(self.nome)
     def save(self):
         db_session.add(self)
+        db_session.commit()
 class Habilidades(Base):
     __tablename__='habilidades'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(80))
+    nome = Column(String(80), index=True)
     programador_id = Column(Integer, ForeignKey('programadores.id'))
     programador = relationship("Programadores")
+    def __repr__(self):
+        return '<Habilidades: {}'.format(self.nome)
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
 def init_db():
     Base.metadata.create_all(bind=engine)
-
+    
 if __name__ == '__main__':
     init_db()
