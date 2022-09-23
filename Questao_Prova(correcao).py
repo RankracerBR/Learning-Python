b29b0f5e061c83133f20f0089efd4e15cb7c2f2a
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-09-22 23:12:51 -0300

    Create Questao_Prova(correcao).py

---
+++ Questao_Prova(correcao).py
@@ -0,0 +1,13 @@
+n = int(input())
+passos = 0 
+
+if n > 0:
+  while n > 1:
+    if n % 2 == 0:
+      n = n /2 
+    else:
+      n = 3*n + 1
+      passos += 1
+  print(passos)
+else:
+  print('impossível calcular')
