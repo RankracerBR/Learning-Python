77f7fa418ead28af813f4cb36e39caaa72591ec6
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-16 01:38:52 -0300

    Create Palavra_Invertida.py

---
+++ Palavra_Invertida.py
@@ -0,0 +1,4 @@
+from Funcao_Pavra_Invertida  import inverter_string  
+palavra = input('Informe uma palavra: ')
+palavra_invertida = inverter_string(palavra)
+print(f'A palavra invertida é: {palavra_invertida}')
