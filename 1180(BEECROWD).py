bc97039efd67677e227250fc797a9adada731c34
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-27 00:35:16 -0300

    Update 1180(BEECROWD).py

--- 1180(BEECROWD).py
+++ 1180(BEECROWD).py
@@ -2,18 +2,17 @@ n = int(input())
 
 a = input()
 
-a = a.split() 
+a = a.split()
 
 for i in range(len(a)):
     a[i] = int(a[i])
     
 menor = a[0]
 posicao = 0
-
-for j in range(1, len(a)): 
-    if a[j] < menor: 
+for j in range(1,len(a)):
+    if a[j] < menor:
         menor = a[j]
-        posicao = j
-    
-print('Menor valor: {}'.format(menor))
-print('Posicao: {}'.format(posicao))
+        posicao = j 
+        
+print(f'Menor valor: {menor}')
+print(f'Posicao: {posicao}')
