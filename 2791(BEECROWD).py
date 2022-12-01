0f7c5c298b73217c2e1d81a8a18e1187fdb6175b
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-12-01 01:14:44 -0300

    Create 2791(BEECROWD).py

---
+++ 2791(BEECROWD).py
@@ -0,0 +1,14 @@
+C1,C2,C3,C4 = input().split()
+C1= int(C1)
+C2 = int(C2)
+C3 = int(C3)
+C4 = int(C4)
+if C1 == 1:
+  c = 1
+elif C2 == 1:
+ c = 2
+elif C3 == 1:
+ c = 3
+elif C4 == 1:
+ c = 4
+print(c)
