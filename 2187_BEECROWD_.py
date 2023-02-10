75b5b0c79e19025a3eba0ba1c1be93c2121a2be8
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-02-10 20:11:48 -0300

    BEECROWD

---
+++ 2187_BEECROWD_.py
@@ -0,0 +1,20 @@
+contador_casos = 1
+while True:
+    v = int(input())
+    if v == 0:
+        break 
+    bits = ['0'] * 4
+    if v >= 50:
+        bits[0] = str(v // 50)
+        v = v % 50
+    if v >= 10:
+        bits[1] = str(v // 10)
+        v = v % 10
+    if v >= 5:
+        bits[2] = str(v // 5)
+        v = v % 5
+    bits[3] = str(v)
+    print(f'Teste {contador_casos}')
+    print(' '.join(bits))
+    print()
+    contador_casos += 1
