76bf28cfa42f27a73949dc9e7d5cb9880c62b118
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-08-08 09:28:44 -0300

    beecrowd

---
+++ 2540(BEECROWD).py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        n = int(input())
+        v = input().split()
+        v = v.count('1')
+        if v/n >= 2/3:
+            print('impeachment')
+        else:
+            print('acusacao arquivada')
+    except EOFError:
+        break
